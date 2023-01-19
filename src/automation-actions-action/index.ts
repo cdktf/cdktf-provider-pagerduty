294721893357753865269da9dcab906ff9d7e33b
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2023-01-19 03:05:31 +0000

    chore: upgrade provider from `2.9.1` to version `2.9.2`

--- src/automation-actions-action/index.ts
+++ src/automation-actions-action/index.ts
@@ -230,7 +230,7 @@ export class AutomationActionsAction extends cdktf.TerraformResource {
       terraformResourceType: 'pagerduty_automation_actions_action',
       terraformGeneratorMetadata: {
         providerName: 'pagerduty',
-        providerVersion: '2.9.1',
+        providerVersion: '2.9.2',
         providerVersionConstraint: '~> 2.5'
       },
       provider: config.provider,
