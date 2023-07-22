272d0fdbe8533e273a6f991ca187b28c8ffbc164
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-22 03:07:49 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/automation-actions-runner/index.ts
+++ src/automation-actions-runner/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/pagerduty/pagerduty/2.15.2/docs/resources/automation_actions_runner
 // generated from terraform resource schema
 
