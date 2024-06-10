c16c1e906435644e20461a8e197aa5788fe663c2
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-10 08:07:16 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6ef397a

--- src/event-orchestration-unrouted/index.ts
+++ src/event-orchestration-unrouted/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.12.2/docs/resources/event_orchestration_unrouted
 // generated from terraform resource schema
 
