0c728b4c006bd40e00cf449e6e1c73a8b437d2e6
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-11 22:10:22 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2e1259a

--- src/data-pagerduty-standards-resources-scores/index.ts
+++ src/data-pagerduty-standards-resources-scores/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.1/docs/data-sources/standards_resources_scores
 // generated from terraform resource schema
 
