ee3a188506b7b10a3539616f339e30ea5025cba9
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-03 09:17:45 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6f08e61

--- src/data-pagerduty-license/index.ts
+++ src/data-pagerduty-license/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.6/docs/data-sources/license
 // generated from terraform resource schema
 
