dc5aa8987051bc8df9e3bb2e30920eeb819db7cf
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-09 03:54:48 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-pagerduty-team-members/index.ts
+++ src/data-pagerduty-team-members/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.1/docs/data-sources/team_members
 // generated from terraform resource schema
 
