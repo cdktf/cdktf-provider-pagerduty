975098804697fc77b218898048f4f42be7e11717
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-10 06:12:33 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-pagerduty-jira-cloud-account-mapping/index.ts
+++ src/data-pagerduty-jira-cloud-account-mapping/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.1/docs/data-sources/jira_cloud_account_mapping
 // generated from terraform resource schema
 
