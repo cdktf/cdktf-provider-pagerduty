9318adb57cd08b115491c7084be999c8d00f7d40
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-23 22:13:53 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@42ce033

--- src/data-pagerduty-jira-cloud-account-mapping/index.ts
+++ src/data-pagerduty-jira-cloud-account-mapping/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.19.2/docs/data-sources/jira_cloud_account_mapping
 // generated from terraform resource schema
 
