87a8c41c5035eaec874bc1fcfa582561ae29ec19
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-28 03:54:47 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/alert-grouping-setting/index.ts
+++ src/alert-grouping-setting/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.19.3/docs/resources/alert_grouping_setting
 // generated from terraform resource schema
 
