8dcb4591efd22e10003147b72767f29987580e5f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 12:15:35 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c005a5

--- src/data-pagerduty-schedule/index.ts
+++ src/data-pagerduty-schedule/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/pagerduty/pagerduty/3.26.0/docs/data-sources/schedule
 // generated from terraform resource schema
 
